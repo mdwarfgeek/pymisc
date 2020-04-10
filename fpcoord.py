1e44e76663c5cdfd664ddaa851ca24fc85214a0e
Jonathan Irwin <jmi@homer.jminet>
2020-04-10 13:30:19 -0400

    Add fpcoord.py (ra/dec to/from standard coordinates).

---
+++ fpcoord.py
@@ -0,0 +1,36 @@
+import math
+import numpy
+
+# RA, Dec to standard coordinates.
+
+def standc(a, d, tpa, tpd):
+  sd = numpy.sin(d)
+  cd = numpy.cos(d)
+  stpd = numpy.sin(tpd)
+  ctpd = numpy.cos(tpd)
+
+  c = numpy.cos(a - tpa)
+
+  denom = stpd * sd + ctpd * cd * c
+
+  xi = numpy.sin(a - tpa) * cd / denom
+  xn = (ctpd * sd - stpd * cd * c) / denom
+
+  return xi, xn
+
+# Standard coordinates to RA, Dec.
+
+def xixn(xi, xn, tpa, tpd):
+  stpd = numpy.sin(tpd)
+  ctpd = numpy.cos(tpd)
+
+  denom = ctpd - xn * stpd
+
+  aa = numpy.arctan2(xi, denom)
+  a = numpy.fmod(aa + tpa, 2*math.pi)
+  a = numpy.where(a < 0, 2*math.pi+a, a)
+
+  d = numpy.arctan2(stpd + xn * ctpd, numpy.hypot(xi, denom))
+
+  return a, d
+
