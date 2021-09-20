84c57c2b707cc759c7b8bc753484c57adec21294
Jonathan Irwin <jmi@homer.jminet>
2021-09-20 14:53:13 -0400

    radec.py: added optional partial keyword for parsing partial strings. Returns the number of characters consumed.

--- radec.py
+++ radec.py
@@ -1,19 +1,22 @@
 import lfa
 
-def convert_radec(radec):
+def convert_radec(radec, partial=False):
   # Convert RA, DEC.  Try : first and then space.
-  ra, rv = lfa.base60_to_10(radec, ':', lfa.UNIT_HR, lfa.UNIT_RAD)
-  if rv < 0:
-    ra, rv = lfa.base60_to_10(radec, ' ', lfa.UNIT_HR, lfa.UNIT_RAD)
-    if rv < 0:
+  ra, rva = lfa.base60_to_10(radec, ':', lfa.UNIT_HR, lfa.UNIT_RAD)
+  if rva < 0:
+    ra, rva = lfa.base60_to_10(radec, ' ', lfa.UNIT_HR, lfa.UNIT_RAD)
+    if rva < 0:
       raise RuntimeError("could not understand radec: " + radec)
     else:
-      de, rv = lfa.base60_to_10(radec[rv:], ' ', lfa.UNIT_DEG, lfa.UNIT_RAD)
-      if rv < 0:
+      de, rvd = lfa.base60_to_10(radec[rva:], ' ', lfa.UNIT_DEG, lfa.UNIT_RAD)
+      if rvd < 0:
         raise RuntimeError("could not understand radec: " + radec)
   else:
-    de, rv = lfa.base60_to_10(radec[rv:], ':', lfa.UNIT_DEG, lfa.UNIT_RAD)
-    if rv < 0:
+    de, rvd = lfa.base60_to_10(radec[rva:], ':', lfa.UNIT_DEG, lfa.UNIT_RAD)
+    if rvd < 0:
       raise RuntimeError("could not understand radec: " + radec)
 
-  return ra, de
+  if partial:
+    return ra, de, rva+rvd
+  else:
+    return ra, de
