e923e9d705af8fed291fbff7ff135b1025b82227
Jonathan Irwin <jmi@homer.jminet>
2021-11-09 13:25:47 -0500

    nlfit: reworked to support box constraints with least_squares.

--- nlfit.py
+++ nlfit.py
@@ -1,7 +1,7 @@
 import numpy
 import scipy.optimize
 
-def nlfit(func, parminit, fixed, y, e_y, escale=True, flag=None, wantcov=False):
+def nlfit(func, parminit, fixed, y, e_y, bounds=None, escale=True, flag=None, wantcov=False):
   """nlfit - non-linear least-squares with tied parameters.
 
 Basic usage:
@@ -21,7 +21,10 @@ function.  The vector "fixed" is a boolean the same length as "parm"
 and "parminit" specifying which parameters are fixed (true) or varied
 (false).  "func" takes two arguments, the parameter vector as a numpy
 array, and an output vector to take the function values at each data
-point.
+point.  An optional argument "bounds" can be used to supply bounds on
+the parameters as a tuple (lower, upper) containing the lower and
+upper bounds in same shape as the parameter vector.  In this case
+scipy.optimize.least_squares is used instead for optimization.
 
 It's a bit of a kludge and lacks useful support for several of the
 usual refinements such as propagating through additional function
@@ -46,6 +49,11 @@ arguments.
 
   # Repack into new vector for fit.
   pinit = parminit[pmap]
+  if bounds is not None:
+    lbounds, ubounds = bounds
+
+    if len(lbounds) > 1:
+      pbounds = (lbounds[pmap], ubounds[pmap])
 
   # Define wrapper to convert calling conventions.
   rwt = 1.0 / e_y
@@ -69,13 +77,26 @@ arguments.
     return f
 
   # Perform minimization.
-  pfit, cov_pfit, infodict, errmsg, ier = scipy.optimize.leastsq(wrap, pinit, full_output=1)
+  if bounds is not None:
+    result = scipy.optimize.least_squares(wrap, pinit, bounds=pbounds)
 
-  if ier < 1 or ier > 4:
-    raise RuntimeError("leastsq: " + errmsg)
+    if not result.success:
+      raise RuntimeError("least_squares: " + result.message)
+
+    pfit = result.x
+    cov_pfit = numpy.linalg.inv(numpy.dot(result.jac.T, result.jac))
+    fvec = wrap(pfit)
+
+  else:
+    pfit, cov_pfit, infodict, errmsg, ier = scipy.optimize.leastsq(wrap, pinit, full_output=1)
+
+    if ier < 1 or ier > 4:
+      raise RuntimeError("leastsq: " + errmsg)
+
+    fvec = infodict["fvec"]
 
   # chi^2 and ndof.
-  chisq = numpy.sum(infodict["fvec"]**2)
+  chisq = numpy.sum(numpy.square(fvec))
   ndof = ndp - pfit.size
 
   if escale and ndof > 0:
